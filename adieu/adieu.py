4b63cc027822fad265d6ef099d1aefb1dae9319f
Han <anilceyhanyeni@gmail.com>
2026-06-22 15:13:23 +0300

    adieu

---
+++ adieu/adieu.py
@@ -0,0 +1,13 @@
+import inflect
+p = inflect.engine()
+
+names = []
+while True:
+    try:
+        name = input("Name: ")
+        names.append(name)
+    except (EOFError, KeyboardInterrupt):
+        break
+count = len(names)
+new_names = p.join(names)
+print(f"Adieu, adieu, to {new_names}" )
