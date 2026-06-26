9165eca2cb8c06a7c3d94c92f53f305ab80ae140
Han <anilceyhanyeni@gmail.com>
2026-06-26 14:28:14 +0300

    um

---
+++ um/um.py
@@ -0,0 +1,11 @@
+import re
+import sys
+def main():
+    print(count(input("Text: ")))
+
+def count(s):
+    matches = re.findall(r"(\Wum\W|\bum\b|\bum\W|\Wum\b)",s, flags=re.IGNORECASE, )
+    return len(matches)
+
+if __name__ == "__main__":
+    main()
